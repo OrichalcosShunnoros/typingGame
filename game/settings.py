21102995a224787297f0d405e1cfb54be4e3c17b
OrichalcosShunnoros <c3146092091@gmail.com>
2024-10-27 17:22:34 -0500

    add init file and settings for game.

---
+++ game/settings.py
@@ -0,0 +1,12 @@
+# Screen constants.
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Colors.
+WHITE = (255, 255, 255)
+BLACK = ( 0, 0, 0)
+
+# Asteroids configuration.
+ASTEROID_LIMIT = 20
+INITIAL_SPEED = 5
+SPEED_INCREASE_INTERVAL = 20
